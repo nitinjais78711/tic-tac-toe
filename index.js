6c845825546eb53ab0f29b93ac4729ec1c70e39d
nitin <nknitinkumar821@gmail.com>
2023-05-19 15:00:53 +0530

    winningLogic

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-let turn="x"
-let changeTurn=()=>{
-  return turn==="x"?"o":"x";
+let turn="X"
+ const changeTurn=()=>{
+  return turn==="X"?"O":"X";
 }
+let isgameOver=false
 // winning logic
 const checkWin = ()=>{
   let boxtext = document.getElementsByClassName('box-item');
@@ -18,15 +19,18 @@ const checkWin = ()=>{
   wins.forEach(e =>{
       if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
           document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
-          isgameover = true 
-           document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+          document.querySelector('.player').innerText = boxtext[e[0]].innerText + " Won"
+          
+          // isgameover = true 
+          document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
           document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
           document.querySelector(".line").style.width = "20vw";
-
-
+          
+          // console.log("won"+turn)
+          
         }
       })
-  }
+    }
 
 
 // changeTurn logic
@@ -38,14 +42,23 @@ Array.from(boxes).forEach(element=>{
       boxitem.innerText=turn;
       turn=changeTurn()
       checkWin()
+      if (!isgameOver){
+        document.getElementsByClassName("info")[0].innerText="Turn for "+ turn;
+      }
     }
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
   })
 }) 
             //------------reset------------  ---
 
             reset.addEventListener('click', ()=>{
-                let boxtexts = document.querySelectorAll('.box-item');
-                Array.from(boxtexts).forEach(element => {
+                let boxtext= document.querySelectorAll('.box-item');
+               Array.from(boxtext).forEach(element => {
                     element.innerText = ""
-                
-                })})
+                    document.getElementsByClassName("player")[0].innertext="Player"
+          
+                })
+                turn ="X"
+                isgameOver=false
+                document.getElementsByClassName("info")[0].innerText="Turn for "+turn;
+              })
